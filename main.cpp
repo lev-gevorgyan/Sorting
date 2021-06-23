ac695ea7dfd42e389a504091554ce05946a75f34
taurus001 <lev.gevorgyan001@gmail.com>
2021-06-23 17:17:34 +0400

    heap sort

--- main.cpp
+++ main.cpp
@@ -5,12 +5,6 @@
 #include "Heap Sort/Heap/Heap.h"
 
 int main() {
-    int arr[] = {1, 15, 40, 32, 63, 9};
-    Heap xh(arr, 6);
-    xh.sort();
 
-    for (int i = 0; i < 6; ++i) {
-        std::cout << arr[i] << "\n";
-    }
     return 0;
 }
